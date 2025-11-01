cc04fd008931b1be2e0d316972502c988a20992e
kailong-chan <tobey98765@gmail.com>
2025-11-02 01:21:31 +0800

    added unit test

--- app/src/test/java/com/example/tipcalculatorpractice/TipCalculatorTest.kt
+++ app/src/test/java/com/example/tipcalculatorpractice/TipCalculatorTest.kt
@@ -1,4 +1,16 @@
 package com.example.tipcalculatorpractice
 
+import org.junit.Test
+import java.text.NumberFormat
+import junit.framework.TestCase.*
+
 class TipCalculatorTest {
+    @Test
+    fun calculateTip_20PercentNoRoundup(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, roundUp = false)
+        assertEquals(expectedTip, actualTip)
+    }
 }
